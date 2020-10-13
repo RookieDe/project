e2a9da389c2e3dd918c03f744cfab8dfbdb70391
chenhongde <634105393@qq.com>
2020-10-13 17:58:34 +0800

    rabbitmq

--- rabbitmq-api/src/main/java/com/project/rabbitmqapi/quickstart/Consumer.java
+++ rabbitmq-api/src/main/java/com/project/rabbitmqapi/quickstart/Consumer.java
@@ -22,13 +22,23 @@ public class Consumer {
         Channel channel = connection.createChannel();
 
         //声明（创建）一个队列
-        String queneName = "test001";
-        channel.queueDeclare(queneName,true,false,false,null);
+
+        String exchangeName = "test_direct_exchange";
+        String exchangeType = "direct";
+        String queueName = "test_direct_queue";
+        String routingKey = "test.direct";
+
+        //表示声明了一个交换机
+        channel.exchangeDeclare(exchangeName,exchangeType,true,false,false,null);
+        //表示声明了一个队列
+        channel.queueDeclare(queueName,false,false,false,null);
+        //建立一个绑定关系
+        channel.queueBind(queueName,exchangeName,routingKey);
 
         //消费消息，推模式
         channel.basicQos(100);
         //设置channel，并创建消费者
-        channel.basicConsume(queneName,true,new DefaultConsumer(channel){
+        channel.basicConsume(queueName,true,new DefaultConsumer(channel){
             //获取消息
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
--- rabbitmq-api/src/main/java/com/project/rabbitmqapi/quickstart/Producer.java
+++ rabbitmq-api/src/main/java/com/project/rabbitmqapi/quickstart/Producer.java
@@ -21,9 +21,11 @@ public class Producer {
         //通过connection创建channel
         Channel channel = connection.createChannel();
 
+        String exchangeName = "test_direct_exchange";
+        String routingKey = "test.direct";
         //通过channel发送数据
         String msg = "Hello,RabbitMQ!";
-        channel.basicPublish("","test001",null,msg.getBytes());
+        channel.basicPublish(exchangeName,routingKey,null,msg.getBytes());
 
         //关闭连接
         channel.close();
